9dc14f6c672cbb5057ac963fa4c53b94adb3af3c
DaehunGwak <rhkrgns56@naver.com>
2023-11-28 03:04:21 +0900

    틱톡 - gaps, sizes 상수 클래스 및 스펙 스크린샷 추가

--- tiktok_clone/lib/main.dart
+++ tiktok_clone/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -14,12 +16,21 @@ class TikTokApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
           seedColor: const Color(0xFFE9435A),
           primary: const Color(0xFFE9435A),
         ),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: Container(
+        padding: const EdgeInsets.all(Sizes.size16),
+        child: const Row(
+          children: [
+            Gaps.h20,
+            Text('hello'),
+          ],
+        ),
+      ),
     );
   }
 }

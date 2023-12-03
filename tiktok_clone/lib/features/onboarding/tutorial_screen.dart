7975af88124f1fd57c4ff692f1d7c76848de6300
DaehunGwak <rhkrgns56@naver.com>
2023-12-04 01:27:40 +0900

    tiktok #5 onboarding: AnimatedCrossFade, AnimatedOpacity 사용으로 변경

--- tiktok_clone/lib/features/onboarding/tutorial_screen.dart
+++ tiktok_clone/lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/onboarding/widgets/tutorial_view.dart';
+
+enum Direction { left, right }
+
+enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,46 +15,75 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.left;
+  Page _page = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      // to the right
+      _direction = Direction.right;
+    }
+
+    if (details.delta.dx < 0) {
+      // to the left
+      _direction = Direction.left;
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _page = Page.second;
+      });
+      return;
+    }
+    if (_direction == Direction.right) {
+      setState(() {
+        _page = Page.first;
+      });
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      // NEW
-      length: 3,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate, // NEW: dragging
+      onPanEnd: _onPanEnd, // NEW: end of dragging
       child: Scaffold(
-        body: const SafeArea(
-          child: TabBarView(
-            // NEW in DefaultTabController
-            children: [
-              TutorialView(
-                title: 'Watch cool videos!',
-                description:
-                    'Videos are personalized for you based on what you watch, like, and share',
-              ),
-              TutorialView(
-                title: 'Fllow the rules!',
-                description:
-                'Videos are personalized for you based on what you watch, like, and share',
-              ),
-              TutorialView(
-                title: 'Enjoy the ride!',
-                description:
-                'Videos are personalized for you based on what you watch, like, and share',
-              ),
-            ],
+        body: SafeArea(
+          child: AnimatedCrossFade(
+            crossFadeState: _page == Page.first
+                ? CrossFadeState.showFirst
+                : CrossFadeState.showSecond,
+            duration: const Duration(milliseconds: 300),
+            firstChild: const TutorialView(
+              title: 'Watch cool videos!',
+              description:
+                  'Videos are personalized for you based on what you watch, like, and share',
+            ),
+            secondChild: const TutorialView(
+              title: 'Follow the rules!',
+              description:
+                  'Take care one of another! please~',
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          elevation: 0,
           child: Container(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size48),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TabPageSelector( // NEW!! in DefaultTabController
-                  color: Colors.white,
-                  selectedColor: Colors.black38,
-                ),
-              ],
+            padding: const EdgeInsets.only(
+              left: Sizes.size48,
+              right: Sizes.size48,
+              bottom: Sizes.size24,
+            ),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _page == Page.first ? 0 : 1,
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).colorScheme.primary,
+                child: const Text('Enter the app!'),
+              ),
             ),
           ),
         ),

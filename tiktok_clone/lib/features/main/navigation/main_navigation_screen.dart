be83695d65e4703f817251ab309aa198ebfb0bd2
DaehunGwak <rhkrgns56@naver.com>
2023-12-05 01:11:06 +0900

    tiktok #6 navigation: CupertinoTabBar로 수정

--- tiktok_clone/lib/features/main/navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main/navigation/main_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -37,23 +38,20 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTab,
-        destinations: [
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.house),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.house),
             label: 'Home',
           ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
             label: 'Search',
           ),
         ],
       ),
+      tabBuilder: (context, index) => screens[index],
     );
   }
 }

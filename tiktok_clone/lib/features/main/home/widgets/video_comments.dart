aa4a3f496a159ded299f7b2c2a2149dc586766f1
DaehunGwak <rhkrgns56@naver.com>
2023-12-07 04:33:22 +0900

    tiktok #8 comments: 3-4: scrollbar, input text action 추가

--- tiktok_clone/lib/features/main/home/widgets/video_comments.dart
+++ tiktok_clone/lib/features/main/home/widgets/video_comments.dart
@@ -13,8 +13,25 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
+  final ScrollController _scrollController = ScrollController();
+
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _stopWriting(BuildContext context) {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _startWrting() {
+    setState(() {
+      _isWriting = true;
+    });
   }
 
   @override
@@ -37,113 +54,166 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size10,
-                horizontal: Sizes.size16,
-              ),
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemCount: 200,
-              itemBuilder: (context, index) {
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: 18,
-                      child: Text('N'),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Daehun Gwak',
-                            style: TextStyle(
-                              color: Colors.grey.shade500,
-                              fontSize: Sizes.size14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Gaps.v3,
-                          const Text(
-                            "That's not it l've seen the same thing but also in a cave",
-                          ),
-                        ],
-                      ),
-                    ),
-                    Gaps.h10,
-                    Column(
+        body: GestureDetector(
+          onTap: () => _stopWriting(context),
+          child: Stack(
+            children: [
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    bottom: Sizes.size96 + Sizes.size20,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                  ),
+                  separatorBuilder: (context, index) => Gaps.v20,
+                  itemCount: 200,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
+                        const CircleAvatar(
+                          radius: 18,
+                          child: Text('N'),
                         ),
-                        Gaps.v2,
-                        Text(
-                          '52.2K',
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                            fontSize: Sizes.size14,
+                        Gaps.h10,
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Daehun Gwak',
+                                style: TextStyle(
+                                  color: Colors.grey.shade500,
+                                  fontSize: Sizes.size14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Gaps.v3,
+                              const Text(
+                                "That's not it l've seen the same thing but also in a cave",
+                              ),
+                            ],
                           ),
+                        ),
+                        Gaps.h10,
+                        Column(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size20,
+                              color: Colors.grey.shade500,
+                            ),
+                            Gaps.v2,
+                            Text(
+                              '52.2K',
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                                fontSize: Sizes.size14,
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                );
-              },
-            ),
-            Positioned(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-              // NEW: 0 으로 하니 같이 끌어올려지지 않음
-              // https://github.com/flutter/flutter/issues/18564#issuecomment-419804357
-              width: size.width,
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: Sizes.size16,
-                    vertical: Sizes.size10,
-                  ),
-                  child: Row(
-                    children: [
-                      const CircleAvatar(
-                        radius: 18,
-                        backgroundColor: Colors.grey,
-                        child: Text(
-                          'N',
-                          style: TextStyle(color: Colors.white),
+                    );
+                  },
+                ),
+              ),
+              Positioned(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+                // NEW: 0 으로 하니 같이 끌어올려지지 않음
+                // https://github.com/flutter/flutter/issues/18564#issuecomment-419804357
+                width: size.width,
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
+                      vertical: Sizes.size10,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.grey,
+                          child: Text(
+                            'N',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
-                      ),
-                      Gaps.h10,
-                      Expanded(
-                        child: TextField(
-                          cursorColor: Theme.of(context).colorScheme.primary,
-                          decoration: InputDecoration(
-                            hintText: 'Write a comment...',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(Sizes.size12),
-                              borderSide: BorderSide.none,
-                            ),
-                            filled: true,
-                            fillColor: Colors.grey.shade200,
-                            contentPadding: const EdgeInsets.symmetric(
-                              horizontal: Sizes.size12,
-                              vertical: Sizes.size10,
+                        Gaps.h10,
+                        Expanded(
+                          child: SizedBox(
+                            height: Sizes.size44,
+                            child: TextField(
+                              onTap: _startWrting,
+                              expands: true,
+                              minLines: null,
+                              maxLines: null,
+                              textInputAction: TextInputAction.newline,
+                              cursorColor:
+                                  Theme.of(context).colorScheme.primary,
+                              decoration: InputDecoration(
+                                hintText: 'Write a comment...',
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(Sizes.size12),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size12,
+                                  vertical: Sizes.size10,
+                                ),
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: Sizes.size14),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      FaIcon(
+                                        FontAwesomeIcons.at,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.gift,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.faceSmile,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      if (_isWriting)
+                                        GestureDetector(
+                                          onTap: () => _stopWriting(context),
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
+                                          ),
+                                        ),
+                                    ],
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

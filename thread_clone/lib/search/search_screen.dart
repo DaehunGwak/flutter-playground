c82991e35a64abedc0a006b38d2b5fcc6951aa31
DaehunGwak <rhkrgns56@naver.com>
2023-12-11 01:46:36 +0900

    #26 Search: refactor Search 스크린

--- thread_clone/lib/search/search_screen.dart
+++ thread_clone/lib/search/search_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:thread_clone/constants/gaps.dart';
 import 'package:thread_clone/constants/sizes.dart';
 import 'package:thread_clone/models/mock/search_mock_data_util.dart';
 
 import '../models/user.dart';
+import 'widget/search_user_list_tile.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -45,97 +45,7 @@ class _SearchScreenState extends State<SearchScreen> {
                   itemCount: _users.length,
                   itemBuilder: (context, index) {
                     final user = _users.elementAt(index);
-                    return ListTile(
-                      titleAlignment: ListTileTitleAlignment.top,
-                      contentPadding: EdgeInsets.zero,
-                      leading: Padding(
-                        padding: const EdgeInsets.only(top: Sizes.size2),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.grey,
-                          foregroundImage: NetworkImage(user.profileImageUrl),
-                        ),
-                      ),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                user.nickname,
-                                style: const TextStyle(
-                                  fontSize: Sizes.size14,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Gaps.h4,
-                              if (user.confirmed)
-                                const Stack(
-                                  children: [
-                                    SizedBox(
-                                      height: Sizes.size14,
-                                      width: Sizes.size14,
-                                      child: Center(
-                                        child: FaIcon(
-                                          FontAwesomeIcons.certificate,
-                                          size: Sizes.size12,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: Sizes.size14,
-                                      width: Sizes.size14,
-                                      child: Center(
-                                        child: Padding(
-                                          padding: EdgeInsets.only(top: Sizes.size2),
-                                          child: FaIcon(
-                                            FontAwesomeIcons.check,
-                                            color: Colors.white,
-                                            size: Sizes.size6,
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                            ],
-                          ),
-                          Text(
-                            user.name ?? '',
-                            style: const TextStyle(
-                              fontSize: Sizes.size14,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Gaps.v10,
-                          Text(
-                            '${user.formattedFollowerCount} followers',
-                            style: const TextStyle(fontSize: Sizes.size12),
-                          )
-                        ],
-                      ),
-                      trailing: Padding(
-                        padding:
-                            const EdgeInsets.symmetric(horizontal: Sizes.size4),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: Sizes.size28,
-                            vertical: Sizes.size5,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(Sizes.size8),
-                            border: Border.all(color: Colors.grey.shade400),
-                          ),
-                          child: const Text(
-                            'Follow',
-                            style: TextStyle(
-                              fontSize: Sizes.size14,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
+                    return SearchUserListTile(user: user);
                   },
                   separatorBuilder: (context, index) => const Divider(
                     height: 0.5,

3db69235bb4f756651d5cc0cf38a36d32f88e2a8
DaehunGwak <rhkrgns56@naver.com>
2023-12-11 23:26:58 +0900

    tiktok #12 profile: 1. SliverAppBar 적용

--- tiktok_clone/lib/features/user/user_profile_screen.dart
+++ tiktok_clone/lib/features/user/user_profile_screen.dart
@@ -10,6 +10,28 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Colors.transparent,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              'assets/images/bg1.jpg',
+              fit: BoxFit.cover,
+            ),
+            title: const Text('Hello!'),
+          ),
+        ),
+      ],
+    );
   }
 }

90b9a50b2736a905a8af77fca8796ff68251215f
DaehunGwak <rhkrgns56@naver.com>
2023-12-12 01:17:47 +0900

    tiktok #12 profile: 5. profile 추가 (feat. VerticalDivider)

--- tiktok_clone/lib/features/user/user_profile_screen.dart
+++ tiktok_clone/lib/features/user/user_profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -14,103 +16,127 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          // snap: true,
-          // 조금이라도 업스크롤 시, appbar 가 나타남
-          // floating: true,
-          // 업스크롤 시, appbar 가 나타남
-          // pinned: true,
-          // 업스크롤 시, appbar 사라지지 않고 작아진채로 고정
-          stretch: true,
-          // 다운 스크롤 하면 appbar 늘어남
-          backgroundColor: Colors.teal,
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-            ],
-            background: Image.asset(
-              'assets/images/bg1.jpg',
-              fit: BoxFit.cover,
-            ),
-            title: const Text('Hello!'),
-          ),
+          title: Text('Daehun'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: FaIcon(
+                FontAwesomeIcons.gear,
+                size: Sizes.size20,
+              ),
+            )
+          ],
         ),
         SliverToBoxAdapter(
           child: Column(
             children: [
-              CircleAvatar(
-                backgroundColor: Colors.red,
-                radius: Sizes.size20,
+              const CircleAvatar(
+                radius: 50,
+                foregroundColor: Colors.blue,
+                foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/12469427?v=4'),
+                child: Text('Daehun'),
               ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Daehun',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size18,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    size: Sizes.size16,
+                    color: Colors.blue.shade500,
+                  ),
+                ],
+              ),
+              Gaps.v24,
+              SizedBox(
+                height: Sizes.size44,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          '97',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v5,
+                        Text(
+                          'Following',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        )
+                      ],
+                    ),
+                    VerticalDivider(
+                      width: Sizes.size32,
+                      thickness: Sizes.size1,
+                      indent: Sizes.size8,
+                      endIndent: Sizes.size8,
+                      color: Colors.grey.shade300,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          '10M',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v5,
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        )
+                      ],
+                    ),
+                    VerticalDivider(
+                      width: Sizes.size32,
+                      thickness: Sizes.size1,
+                      indent: Sizes.size8,
+                      endIndent: Sizes.size8,
+                      color: Colors.grey.shade300,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          '168.2M',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v5,
+                        Text(
+                          'Likes',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
-        ),
-        SliverFixedExtentList(
-          itemExtent: 100, // 각 아이템 크기
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Center(child: Text("Item $index")),
-            ),
-          ),
-        ),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          pinned: true,
-          floating: true
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Center(child: Text("Item $index")),
-            ),
-          ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        ),
+        )
       ],
     );
-  }
-}
-
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.indigo,
-      child: const FractionallySizedBox(
-        heightFactor: 1.0,
-        child: Center(
-          child: Text(
-            'sliver persistent header',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ),
-    );
-  }
-
-  // maxheight
-  @override
-  double get maxExtent => 100;
-
-  // minheight
-  @override
-  double get minExtent => 50;
-
-  //
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
   }
 }

82b64f82011f5d06490c8ae9b10ddd7382651fb5
DaehunGwak <rhkrgns56@naver.com>
2023-12-12 00:11:57 +0900

    tiktok #12 profile: 2. SliverAppBar + SliverFixedExtentList 적용

--- tiktok_clone/lib/features/user/user_profile_screen.dart
+++ tiktok_clone/lib/features/user/user_profile_screen.dart
@@ -13,10 +13,11 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          floating: true,
-          stretch: true,
-          pinned: true,
-          backgroundColor: Colors.transparent,
+          snap: true, // 조금이라도 업스크롤 시, appbar 가 나타남
+          floating: true, // 업스크롤 시, appbar 가 나타남
+          // pinned: true, // 업스크롤 시, appbar 사라지지 않고 작아진채로 고정
+          stretch: true, // 다운 스크롤 하면 appbar 늘어남
+          backgroundColor: Colors.teal,
           collapsedHeight: 80,
           expandedHeight: 200,
           flexibleSpace: FlexibleSpaceBar(
@@ -29,6 +30,16 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               fit: BoxFit.cover,
             ),
             title: const Text('Hello!'),
+          ),
+        ),
+        SliverFixedExtentList(
+          itemExtent: 100, // 각 아이템 크기
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Center(child: Text("Item $index")),
+            ),
           ),
         ),
       ],

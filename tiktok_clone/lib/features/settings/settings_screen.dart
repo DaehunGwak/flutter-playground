f19a3d3789efee28a5178f8fd9c9dc7cbc156d17
DaehunGwak <rhkrgns56@naver.com>
2023-12-12 22:16:13 +0900

    tiktok #13 setting: 2. showAboutDialog, AboutListTile 테스트
    - 앱 출시에 소프트웨어 라이센스 정보는 필히 넣어야 하므로,
    flutter 에서 자동으로 만들어주는 위젯 위 2개가 존재

--- tiktok_clone/lib/features/settings/settings_screen.dart
+++ tiktok_clone/lib/features/settings/settings_screen.dart
@@ -10,44 +10,25 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const Column(
+      body: ListView(
         children: [
-          CupertinoActivityIndicator(
-            radius: 40,
-          ), // ios style loading indicator
-          CircularProgressIndicator(), // android style loading indicator
-          CircularProgressIndicator.adaptive(), // 플랫폼에 따라 바뀌는 loading indicator
+          ListTile(
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationVersion: '1.0',
+              applicationLegalese: "All rights reserved. Please don't copy me.",
+            ),
+            title: const Text(
+              'About',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text('About this app....'),
+          ),
+          const AboutListTile(),
         ],
       ),
-      // body: ListWheelScrollView(
-      //   diameterRatio: 1.5, // 휠 펴지는 정도
-      //   offAxisFraction: 2,
-      //   // useMagnifier: true, // 중앙 돋보기
-      //   // magnification: 1.5, // 중앙 돋보기 배율
-      //   itemExtent: 200, // item 높이
-      //   children: [
-      //     for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1])
-      //       FractionallySizedBox(
-      //         widthFactor: 1.0,
-      //         child: Container(
-      //           color: Colors.teal,
-      //           alignment: Alignment.center,
-      //           child: const Text(
-      //             'Pick me',
-      //             style: TextStyle(
-      //               fontSize: Sizes.size32,
-      //               color: Colors.white,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //   ],
-      // ),
-      // body: const Column(
-      //   children: [
-      //     // CloseButton() // NEW
-      //   ],
-      // ),
     );
   }
 }

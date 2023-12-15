8388f925c9122fb9c9ef60adfc2711a5f98ecadc
DaehunGwak <rhkrgns56@naver.com>
2023-12-15 23:22:16 +0900

    tiktok #19 video: 7. VideoPreviewScreen + gal 패키지 추가

--- tiktok_clone/lib/features/video/video_preview_screen.dart
+++ tiktok_clone/lib/features/video/video_preview_screen.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gal/gal.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -18,6 +21,21 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+
+  bool _isSaved = false;
+
+  Future<void> _saveVideoToGallery() async {
+    if (_isSaved) return;
+
+    Gal.putVideo(
+      widget.video.path,
+      album: 'Tiktok Clone',
+    );
+
+    setState(() {
+      _isSaved = true;
+    });
+  }
 
   Future<void> _initVideo() async {
     _videoPlayerController =
@@ -36,10 +54,30 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   }
 
   @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Preview video'),
+        actions: [
+          GestureDetector(
+            onTap: _saveVideoToGallery,
+            child: Container(
+              padding: const EdgeInsets.only(
+                right: Sizes.size14,
+                top: Sizes.size20,
+              ),
+              child: FaIcon(
+                _isSaved ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+              ),
+            ),
+          ),
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)

bf84ad7925221bec6ed8e6976f598eb545d5f60b
DaehunGwak <rhkrgns56@naver.com>
2023-12-28 00:54:01 +0900

    tiktok #26 upload: firebase function + thumbnail 추출 및 데이터 업데이트 추가

--- tiktok_clone/functions/src/index.ts
+++ tiktok_clone/functions/src/index.ts
@@ -3,9 +3,45 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export const onVideoCreated = functions.region("asia-northeast3")
+export const onVideoCreated = functions.runWith({memory: "512MB"})
+    .region("asia-northeast3")
     .firestore
     .document("videos/{videoId}")
     .onCreate(async (snapshot, context) => {
-        await snapshot.ref.update({"hello": "from functions"});
+        const spawn = require('child-process-promise').spawn;
+        const video = snapshot.data();
+
+        const filePath = `/tmp/${snapshot.id}.jpg`;
+
+        await spawn("ffmpeg", [
+            "-i",
+            video.fileUrl,
+            "-ss",
+            "00:00:01.000",
+            "-vframes",
+            "1",
+            "-vf",
+            "scale=150:-1",
+            filePath,
+        ]);
+
+        const storage = admin.storage();
+        const [file, _] = await storage.bucket().upload(filePath, {
+            destination: `thumbnails/${snapshot.id}.jpg`,
+        });
+
+        await file.makePublic();
+        await snapshot.ref.update({
+            thumbnailUrl: file.publicUrl(),
+        });
+
+        const db = admin.firestore();
+        await db.collection("users")
+            .doc(video.creatorUid)
+            .collection("videos")
+            .doc(snapshot.id)
+            .set({
+                thumbnailUrl: file.publicUrl(),
+                videoId: snapshot.id,
+            });
     });

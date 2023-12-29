9d27bc0c4baae6e0e11e07252a26e1c43a4d9c50
DaehunGwak <rhkrgns56@naver.com>
2023-12-30 02:10:08 +0900

    46 threads - search posts: prefix 검색 기능 구현

--- thread_clone/lib/search/search_contents_screen.dart
+++ thread_clone/lib/search/search_contents_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:thread_clone/models/feed.dart';
 import 'package:thread_clone/search/view_models/search_threads_view_model.dart';
 import 'package:thread_clone/timeline/widgets/feed_widget.dart';
 
-class SearchContentsScreen extends ConsumerWidget {
+class SearchContentsScreen extends ConsumerStatefulWidget {
   const SearchContentsScreen({
     super.key,
     required this.searchQuery,
@@ -12,21 +13,42 @@ class SearchContentsScreen extends ConsumerWidget {
   final String searchQuery;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SearchContentsScreen> createState() =>
+      _SearchContentsScreenState();
+}
+
+class _SearchContentsScreenState extends ConsumerState<SearchContentsScreen> {
+  bool isLoading = true;
+  List<Feed> feeds = [];
+  Future<void> _doSearch() async {
+    feeds = await ref
+        .read(searchThreadsViewModel.notifier)
+        .searchContent(widget.searchQuery);
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _doSearch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(searchQuery),
+        title: Text(widget.searchQuery),
       ),
-      body: ref.watch(searchThreadsViewModel).isLoading
+      body: isLoading
           ? const Center(
               child: CircularProgressIndicator.adaptive(),
             )
-          : _buildFeedListView(ref),
+          : _buildFeedListView(),
     );
   }
 
-  Widget _buildFeedListView(WidgetRef ref) {
-    final feeds = ref.read(searchThreadsViewModel).value ?? List.empty();
+  Widget _buildFeedListView() {
     return feeds.isEmpty
         ? const Center(
             child: Text("It's empty. 🫥"),

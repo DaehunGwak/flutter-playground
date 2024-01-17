20289d445074f9a9653dca5ce021ab689f332344
DaehunGwak <rhkrgns56@naver.com>
2024-01-18 01:49:38 +0900

    ani 05. music player: AnimatedSwitcher + blur

--- animation_music_player/lib/screens/music_player_screen.dart
+++ animation_music_player/lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:animation_music_player/models/movie_model.dart';
 import 'package:flutter/material.dart';
 
@@ -15,53 +17,104 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     viewportFraction: 0.8,
   );
 
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
-          PageView.builder(
-            controller: _pageController,
-            itemCount: tracks.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 350,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(tracks[index].imageUrl),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    tracks[index].title,
-                    style: const TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    tracks[index].artist,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
+          _buildBackground(),
+          _buildTrackWidgets(),
         ],
       ),
+    );
+  }
+
+  AnimatedSwitcher _buildBackground() {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 500),
+      child: Container(
+        key: ValueKey("background-image-$_currentPage"),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(tracks[_currentPage].imageUrl),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 25,
+            sigmaY: 25,
+          ),
+          child: Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+        ),
+      ),
+    );
+  }
+
+  PageView _buildTrackWidgets() {
+    return PageView.builder(
+      controller: _pageController,
+      onPageChanged: _onPageChanged,
+      itemCount: tracks.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.4),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 8),
+                  )
+                ],
+                image: DecorationImage(
+                  image: NetworkImage(tracks[index].imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              tracks[index].title,
+              style: const TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              tracks[index].artist,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }

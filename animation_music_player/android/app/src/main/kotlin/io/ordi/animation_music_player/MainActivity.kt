b2c77260e2721a81d4232671f438e3b455e2ce8d
DaehunGwak <rhkrgns56@naver.com>
2024-01-18 00:51:50 +0900

    ani 05. music player: 프로젝트 초기화

---
+++ animation_music_player/android/app/src/main/kotlin/io/ordi/animation_music_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.ordi.animation_music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animation_music_player/lib/screens/music_player_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerScreen extends StatefulWidget {
+  const MusicPlayerScreen({super.key});
+
+  @override
+  State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(),
+    );
+  }
+}

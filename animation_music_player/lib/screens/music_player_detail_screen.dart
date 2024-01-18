5ee5b5b860b707ad5caaaa886fe7aa60048fec64
DaehunGwak <rhkrgns56@naver.com>
2024-01-19 02:02:33 +0900

    ani 05. music player: Marquee (SlideTransition)

--- animation_music_player/lib/screens/music_player_detail_screen.dart
+++ animation_music_player/lib/screens/music_player_detail_screen.dart
@@ -16,14 +16,25 @@ class MusicPlayerDetailScreen extends StatefulWidget {
 }
 
 class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late final AnimationController _progressController = AnimationController(
     vsync: this,
     duration: widget.track.runtime,
   )..repeat(reverse: true);
 
+  late final AnimationController _marqueeController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+  );
+
+  late final Animation<Offset> _marqueeTween = Tween(
+    begin: const Offset(0, 0),
+    end: const Offset(-0.5, 0),
+  ).animate(_marqueeController);
+
   @override
   void dispose() {
+    _marqueeController.dispose();
     _progressController.dispose();
     super.dispose();
   }
@@ -31,9 +42,7 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.track.title),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -43,6 +52,10 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
               height: 50,
             ),
             _buildProgress(),
+            const SizedBox(
+              height: 30,
+            ),
+            _buildTrackText(),
           ],
         ),
       ),
@@ -132,6 +145,45 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildTrackText() {
+    if (widget.track.title.length > 23) {
+      _marqueeController.repeat(reverse: true);
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        children: [
+          SlideTransition(
+            position: _marqueeTween,
+            child: Text(
+              widget.track.title,
+              maxLines: 1,
+              softWrap: false,
+              overflow: TextOverflow.visible,
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            widget.track.artist,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

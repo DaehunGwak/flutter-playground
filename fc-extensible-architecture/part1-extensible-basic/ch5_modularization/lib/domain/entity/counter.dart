1981641578df9d490573931a1348e1e5705ddfa3
DaehunGwak <rhkrgns56@naver.com>
2024-05-15 19:12:05 +0900

    fc-ex: part 1.5. module - init project

---
+++ fc-extensible-architecture/part1-extensible-basic/ch5_modularization/lib/domain/entity/counter.dart
@@ -0,0 +1,13 @@
+class CounterModel {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+  }
+
+  void decrement() {
+    _counter--;
+  }
+}
---
+++ fc-extensible-architecture/part1-extensible-basic/ch5_modularization/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:ch5_modularization/domain/domain.dart';
+import 'package:ch5_modularization/presentation/presentation.dart';
+import 'package:ch5_modularization/ui/screen/screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: CounterView(
+        counterViewModel: CounterViewModel(CounterModel()),
+        counterModeViewModel: CounterModeViewModel(CounterModeModel()),
+      ),
+    );
+  }
+}

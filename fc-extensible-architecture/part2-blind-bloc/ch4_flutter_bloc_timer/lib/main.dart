7f5ec03c5b88e00216bdd329d7c9d4217fc889fb
DaehunGwak <rhkrgns56@naver.com>
2024-05-22 02:57:15 +0900

    fc-ex: part 4.3. flutter_bloc_timer - init project

---
+++ fc-extensible-architecture/part2-blind-bloc/ch4_flutter_bloc_timer/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Timer'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+      ),
+    );
+  }
+}

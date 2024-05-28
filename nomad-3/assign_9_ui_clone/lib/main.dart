7daf94d86911d1c793e547299edabbbaaad779ec
DaehunGwak <rhkrgns56@naver.com>
2024-05-29 01:18:50 +0900

    nomad3: assign 7 UI Clone - project 초기화 & dark mode 적용

---
+++ nomad-3/assign_9_ui_clone/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:assign_9_ui_clone/views/pages/home_page.dart';
+import 'package:assign_9_ui_clone/views/shared/color_scheme.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        colorScheme: darkColorScheme,
+        useMaterial3: true,
+        appBarTheme: darkAppBarTheme,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ nomad-3/assign_9_ui_clone/lib/views/shared/color_scheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final darkColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.black,
+  brightness: Brightness.dark,
+  surface: const Color.fromRGBO(32, 32, 32, 0),
+);
+
+const darkAppBarTheme = AppBarTheme(
+  backgroundColor: Colors.transparent,
+);

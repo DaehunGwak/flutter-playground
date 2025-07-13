61b5a211699619bd2954be463960ac4c5f25eef2
DaehunGwak <rhkrgns56@naver.com>
2025-07-13 18:38:11 +0900

    feat: [fc15 part6-ch09] 프로젝트 초기화 및 예제 코드 추가

---
+++ boilerplate_firebase_kakao/android/app/src/main/kotlin/io/ordi/boilerplate/boilerplate_firebase_kakao/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.ordi.boilerplate.boilerplate_firebase_kakao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ boilerplate_firebase_kakao/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:boilerplate_firebase_kakao/presentation/pages/user/user_page.dart';
+import 'package:flutter/material.dart';
+
+import 'core/theme/theme_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: CustomThemeData.themeData,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return UserPage();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UserPage(),
+        ],
+      ),
+    );
+  }
+}
